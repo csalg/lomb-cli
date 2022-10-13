c1c8aadc1329f37761a8e07341ca198b3d3c37ef
Carlos Perez-Guerra <cpg@raffle.ai>
2022-10-14 00:15:32 +0200

    print out urls to stdout

---
+++ cmd/lomb/main.go
@@ -0,0 +1,23 @@
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/csalg/lomb-cli/pkg/revise"
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := &cli.App{
+		Name:  "lomb",
+		Usage: "Lomb CLI",
+		Commands: []*cli.Command{
+			revise.Cmd(),
+		},
+	}
+	if err := app.Run(os.Args); err != nil {
+		fmt.Fprint(os.Stderr, "Error: "+err.Error())
+	}
+}
